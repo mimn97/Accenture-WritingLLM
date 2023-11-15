3b97b8b9fe3a443f3a1bf4ddde16feb20e7b533d
Minhwa Lee <59892379+mimn97@users.noreply.github.com>
2023-11-14 18:53:06 -0600

    resolve st.secrets

--- codes/pages/3_Annotate Your Edits.py
+++ codes/pages/3_Annotate Your Edits.py
@@ -10,8 +10,8 @@ import os
 # aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 # aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-aws_access_key_id = st.secrets('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = st.secrets('AWS_SECRET_ACCESS_KEY')
+aws_access_key_id = st.secrets["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
 
 def split_html_into_sentences(html_text):
     html_sentences = []
